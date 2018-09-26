edbfb4476b8a2f40203dd87246faec6408089c27
Sergiy Lavryk <jagermesh@gmail.com>
2018-09-26 17:07:16 -0400

    fx

--- demo/index.php
+++ demo/index.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once(dirname(__DIR__) . '/LatexTools.php');
+require_once(dirname(__DIR__) . '/src/LatexTools.php');
 
 $latexTools = new LatexTools();
 $latexTools->renderIntoResponse('(\frac{\beta }{\mu})^\beta {\Gamma(\beta )} \,  e^{-\frac{V\,\beta }{\mu }} \label{gamma}');
